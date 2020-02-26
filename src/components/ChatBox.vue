b2ff7bf0b42eeef52514a6fa9afde99a6a8ad164
wemali05 <alfredwemali05@gmail.com>
2020-02-26 21:09:19 +0300

    chatbot update

--- src/components/ChatBox.vue
+++ src/components/ChatBox.vue
@@ -1,10 +1,11 @@
 <template>
   <section class="chat-box">
-    <div class="chat-bot-list-container">
+    <div class="chat-box-list-container" ref="chatbox">
         <ul class="chat-box-list">
             <li class="message"
             v-for="(message, index) in messages"
             :key="index"
+            :class="message.author"
             >
              <p><span>{{ message.text }}</span></p>   
             </li>
@@ -35,18 +36,93 @@ export default {
              text: this.message,
              author: 'client'
          })
+         this.message = ''   
+
          this.$axios.get('https://www.cleverbot.com/getreply?key=CC8uqcCcSO3VsRFvp5-uW5Nxvow&input=${message}')
          .then( res => {
              this.messages.push({
                  text: res.data.output,
                  author: 'server'
              })
+               this.$nextTick(() => {
+               this.$refs.chatbox.scrollTop = this.$refs.chatbox.scrollHeight
+           })
          })
      }
- },
+ }
 }
 </script>
 
-<style scoped lang="SCSS">
+<style scoped lang="scss">
+.chat-box,
+.chat-box-list {
+    display: flex;
+    flex-direction: column;
+    list-style-type: none;
+}
+.chat-box{
+    border: 1px solid #999;
+    width: 50vw;
+    height: 50vh;
+    border-radius: 4px;
+    margin-left: auto;
+    margin-right: auto;
+    align-items: space-between;
+    justify-content: space-between;
+}
+
+.chat-box-list-container{
+    overflow: scroll;
+}
+
+.chat-box-list{
+    margin: 10px;
+    padding-left: 0;
+
+    span {
+        padding: 8px;
+        color: white;
+        border-radius: 4px;
+    }
+
+    .server {
+       span{
+            background: green
+       }
+       p{
+           float: left
+       }
+    }
+    .client {
+       span {
+            background: blue;
+       }
+       p{
+           float: right;
+       }
+    }
+}
+
+.chat-inputs{
+    display: flex;
+
+    input{
+        line-height: 3;
+        width: 100%;
+        border: 1px solid #999;
+        border-left: none;
+        border-bottom: none;
+        border-right: none;
+        border-bottom-left-radius: 4px;
+        padding-left: 15px;
+    }
+
+    button{
+        width: 145px;
+        color: white;
+        background-color: green;
+        border-bottom-right-radius: 4px;
+    }
+}
 
 </style>
